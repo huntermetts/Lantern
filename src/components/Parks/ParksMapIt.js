24dea3303385fe85687898e6625a33e0bc5c100f
Hunter Metts <huntermettsdev@gmail.com>
2019-02-07 12:07:12 -0600

    mapIt settup

--- src/components/Parks/ParksMapIt.js
+++ src/components/Parks/ParksMapIt.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import "./ParksCampingPage.css"
 import { Link } from "react-router-dom";
 import backArrow from './backArrow.png'
+import Iframe from 'react-iframe'
 // import dividerLine from "./dividerLine.png"
 
 
@@ -14,6 +15,17 @@ export default class ParksMapIt extends Component {
             <section className="mapItPage">
             <img src={backArrow} onClick={() => this.props.history.push("/parksMainPage")} className="mapBackArrow" alt="backArrow"></img>
             <h3 className="descriptionHeader"><strong>{this.props.parkName}</strong></h3>
+            <div className="iFrameContainer">
+            <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
+                width="350px"
+                height="500px"
+                id="map"
+                className="iFrameMap"
+                display="initial"
+                position="relative"
+                allowFullScreen
+            />
+            </div>
             </section>
             </React.Fragment>
         )
